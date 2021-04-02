e9d18e777b20604196fc8348d77136a1c0eced50
taro <groveleffect@gmail.com>
2021-04-02 16:41:57 +0900

    経費申請画面の作成（画面のみ）

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,4 @@
 class ExpensesController < ApplicationController
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'homes#home'
+
   get 'home', to: 'homes#home'
+  get 'expenserequest', to: 'expenses#index'
+
+  resources :expenses, except: :index
 end
