816a7e86618afb5de98130d73bce862acdd05655
taro <groveleffect@gmail.com>
2021-04-02 17:10:37 +0900

    給与明細画面の作成（画面のみ）

---
+++ app/controllers/pays_controller.rb
@@ -0,0 +1,4 @@
+class PaysController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'home', to: 'homes#home'
   get 'expenserequest', to: 'expenses#index'
   get 'attendance', to: 'attendances#index'
+  get 'payslip', to: 'pays#index'
 
   resources :expenses, except: :index
   resources :attendances, except: :index
