8c2b7f33ad6eba9464f4f6d3008dffb97e4da32b
taro <groveleffect@gmail.com>
2021-04-08 09:54:00 +0900

    ログイン機能の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    home_path
+  end
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "before_layouts"
+    else
+      "application"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   root to: 'homes#home'
 
+  # devise_for :users
+  devise_for :users, skip: :all
+  devise_scope :user do
+    get 'login' => 'devise/sessions#new', as: :new_user_session
+    post 'login' => 'devise/sessions#create', as: :user_session
+    delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+    get 'changepassword' => 'devise/passwords#edit', as: :edit_user_password
+    post 'changepassword' => 'devise/passwords#update', as: :user_password
+  end
+
   get 'home', to: 'homes#home'
   get 'expenserequest', to: 'expenses#index'
   get 'attendance', to: 'attendances#index'
